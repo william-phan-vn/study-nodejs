a9425194c7aff947ca53fbdaa4d7ba14cc718c3b
william-phan <hoangviet191@gmail.com>
2025-01-21 15:18:18 +0700

    express routing

--- express-js/app.js
+++ express-js/app.js
@@ -2,7 +2,24 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express!');
+  res.send('Welcome to My Website!');
+});
+
+app.get('/user/:id', (req, res) => {
+    res.send('User ID: ' + req.params.id);
+});
+
+app.get('/search', (req, res) => {
+    const query = req.query.query || 'No search term provided';
+    res.send(`Search for: ${query}`);
+});
+
+app.get('/about', (req, res) => {
+    res.send('About us');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Contact us');
 });
 
 app.listen(3000, () => {
